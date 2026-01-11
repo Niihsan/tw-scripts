477ac7b68891c8a15f40fca82ae9e7801bfd5712
Niihsan <36795471+Niihsan@users.noreply.github.com>
2026-01-10 20:34:34 -0400

    Update frontlineStacks.js

--- frontlineStacks.js
+++ frontlineStacks.js
@@ -19,8 +19,8 @@ var scriptConfig = {
     scriptData: {
         prefix: 'frontlineStacksPlanner',
         name: `Frontline Stacks Planner`,
-        version: 'v1.0.3-Niihsan',
-        author: 'RedAlert + Niihsan',
+        version: 'v1.0.3',
+        author: 'RedAlert & Niihsan',
         authorUrl: 'https://twscripts.dev/',
         helpLink:
             'https://forum.tribalwars.net/index.php?threads/frontline-stacks-planner.291478/',
